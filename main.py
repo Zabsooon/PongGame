8d05b1d9bf45fa1183e4e3eac0bc9ec5b304d736
Jakub Żabski <56307289+Zabsooon@users.noreply.github.com>
2022-03-24 17:19:08 +0000

    Creating paddles movement

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from turtle import Screen
+from paddle import Paddle
+
+STARTING_X1 = -350
+STARTING_Y = 0
+STARTING_X2 = 350
 
 screen = Screen()
-screen.setup(width=1200, height=800)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
-# screen.tracer(0)
-# screen.update()
+screen.tracer(0)
+screen.listen()
+
+
+paddle_l = Paddle((STARTING_X1, STARTING_Y))
+paddle_r = Paddle((STARTING_X2, STARTING_Y))
+
+game_is_on = True
+while game_is_on:
+
+    screen.onkeypress(paddle_l.go_up, "w")
+    screen.onkeypress(paddle_r.go_up, "Up")
+
+    screen.onkeypress(paddle_l.go_down, "s")
+    screen.onkeypress(paddle_r.go_down, "Down")
+
+    screen.update()
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.turtlesize(6, 1)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        self.sety(self.ycor() + 20)
+
+    def go_down(self):
+        self.sety(self.ycor() - 20)
